af3eadd3d76a23d557fe0ab45d72b66945c90cc1
Dzmitry Liashko <dzliashko@gmail.com>
2025-04-25 13:10:00 +0300

    Add: Is_he_gonna_survive

---
+++ Is_he_gonna_survive.py
@@ -0,0 +1,27 @@
+# https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/python
+
+# A hero is on his way to the castle to complete his mission. However, he's
+# been told that the castle is surrounded with a couple of powerful dragons!
+# each dragon takes 2 bullets to be defeated, our hero has no idea how many
+# bullets he should carry.. Assuming he's gonna grab a specific given number
+# of bullets and move forward to fight another specific given number of dragons,
+# will he survive?
+
+# Return true if yes, false otherwise :)
+
+
+# def hero(bullets, dragons):
+#     return bullets - (dragons * 2) >= 0
+
+
+# Best practice
+def hero(bullets, dragons):
+    return bullets - (dragons * 2) >= 0
+
+
+print(hero(10, 5))
+print(hero(7, 4))
+print(hero(4, 5))
+print(hero(100, 40))
+print(hero(1500, 751))
+print(hero(0, 1))
